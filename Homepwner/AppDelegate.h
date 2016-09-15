0f840a673f12d23f3f6a25ece04d4c5c2b5d08bd
Princess Sampson <ps@kingonomics.com>
2016-09-15 06:53:09 -0400

    Initial Commit

---
+++ Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by Princess Sampson on 9/15/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Homepwner/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Homepwner
+//
+//  Created by Princess Sampson on 9/15/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
